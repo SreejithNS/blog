f5e40f9b2921e9d6e84bf749c02482403b9fcdfe
SreejithNS <nssreejith229@gmail.com>
2022-06-22 17:28:11 +0530

    Added icon fonts and custom fonts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,41 +6,35 @@ type Props = {
 };
 export default function Layout({ children }: Props) {
   return (
-    <div className="root">
+    <div>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
+
+        {/* For MUI Material Font Icons */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+
+        {/* Custom Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=PT+Sans&display=swap"
+          rel="stylesheet"
+        />
+
+        {/* Custom Fonts Usage
+            font-family: 'Open Sans', sans-serif; // Headings
+            font-family: 'PT Sans', sans-serif;   // Body
+        */}
+
       </Head>
-      <nav>
-        <Navigation />
-      </nav>
       <main>{children}</main>
-      <style jsx>
-        {`
-          .root {
-            display: block;
-            padding: 4rem 0;
-            box-sizing: border-box;
-            height: 100%;
-          }
-          main {
-            display: flex;
-            min-height: 100%;
-          }
-          @media (min-width: 769px) {
-            .root {
-              display: flex;
-              flex: 1 0 auto;
-            }
-            main {
-              flex: 1 0 auto;
-            }
-          }
-        `}
-      </style>
     </div>
   );
 }
