f8ca53798290ee5a33a1162927d6355e289d8dfb
SreejithNS <nssreejith229@gmail.com>
2022-06-22 17:33:46 +0530

    Module declaration for sass files

--- global.d.ts
+++ global.d.ts
@@ -22,3 +22,8 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
