dae6b445147fd28076b611edaa380d01d689d031
SreejithNS <nssreejith229@gmail.com>
2022-07-31 23:56:04 +0530

    Minor Accessibility and performance tweaks
    - Altered font loading strategy
    - Added lang attribute in HTMl for accessibility.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
+                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin="true" />
+                    <link rel='preload' as='style' href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@305;430;500;700&family=PT+Sans&display=swap" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@305;430;500;700&family=PT+Sans&display=swap"
                         rel="stylesheet"
